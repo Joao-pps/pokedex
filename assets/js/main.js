befdfd81338e4a4be4bca486a6f5678184f14825
João <jppssousamagalhaes@outlook.com>
2023-04-29 16:12:18 -0300

    adição de limitação de pokemons da 1 geração.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,47 @@
-/* 
-    Abaixo vai ter uma função que vai ter um processamento assincrono siginificando que vai ser um processo que não vai ter uma sequencia de execução
-    o seu retorno vai ser uma (promise) de uma (response), responso representa uma resposta a uma solicitação HTTP, que representara o header do meu HTTP.
-*/
-function pokemonConvertToHtlm(pokemon) {
-    return `
-        <article class="pokemon pokemon__bulbasour ${pokemon.type}">
-            <span class="number">${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detalhes">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img class="article__imagem" src="${pokemon.photo}" alt="${pokemon.name}">
-            </div>
-        </article>`
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 10;  
+let offset = 0;
+let maxRecord = 151;  //quantidades de pokemons da primeira geração
+
+function loadPokemonsItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <article class="pokemon pokemon__bulbasour ${pokemon.type}">
+                <span class="number">${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detalhes">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img class="article__imagem" src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </article>`
+        ).join('');  //simplificando tudo.
+        pokemonList.innerHTML += newHtml
+        console.log(pokemons)
+    })
+
 }
 
-const pokemonList = document.getElementById("pokemonList");
+loadPokemonsItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(pokemonConvertToHtlm).join('');  //simplificando tudo.
-    console.log(pokemons)
-})
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    debugger
+    const qtdRecordNexPage = offset + limit;
+    if (qtdRecordNexPage >= maxRecord) {
+        const newLimit = maxRecord - offset;
+        loadPokemonsItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonsItens(offset, limit)
+    }
+}) 
+
+
+
+
+
 
